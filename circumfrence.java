4f3b710720d52a9176bafdc9a99d690fc4994cd7
Aman Sahay <142723979+amansahaypersonal@users.noreply.github.com>
2025-06-22 18:02:56 +0530

    Add files via upload

---
+++ circumfrence.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class circumfrence {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the radius of the circle: ");
+        Double radii = sc.nextDouble();
+
+        circumfrenceOfCircle(radii);
+
+        sc.close();
+
+    }
+
+    public static void circumfrenceOfCircle(Double radii) {
+        Double circum = 2 * 3.14 * radii;
+        System.out.println(circum);
+    }
+}
