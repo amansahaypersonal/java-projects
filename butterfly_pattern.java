5036c0598081905c0b4ed1715e10626e485cb245
Aman Sahay <142723979+amansahaypersonal@users.noreply.github.com>
2025-06-22 11:59:21 +0530

    Add files via upload

---
+++ butterfly_pattern.java
@@ -0,0 +1,46 @@
+ class butterfly_pattern {
+    public static void main(String[] args){
+        int n = 5;
+    //upper half
+        for(int i = 1; i <= n; i++){
+            //first half
+            for(int j = 1;j<= i;j++){
+                System.out.print("*");
+
+            }
+             //spaces
+             int spaces = 2 * (n - i);
+            for(int j = 1; j <= spaces ;j++){
+                System.out.print(" ");
+            }
+             //Seconf half
+              for(int j = 1;j<= i;j++){
+                System.out.print("*");
+
+            }
+            System.out.println(" ");
+            
+        }
+        // lower half
+        for(int i = 5; i >= 1; i--){
+
+            //first half
+            for(int j = 1;j<= i;j++){
+                System.out.print("*");
+
+            }
+
+            //spaces
+            for(int j = 1; j <= 2 * (n - i);j++){
+                System.out.print(" ");
+            }
+
+            //seconf half
+              for(int j = 1;j<= i;j++){
+                System.out.print("*");
+
+            }
+               System.out.println(" ");
+        }
+    }    
+}
