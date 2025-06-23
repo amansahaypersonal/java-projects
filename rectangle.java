ecf6c6ad51ce6e0285dd932d2c8ac326b3cf6147
Aman Sahay <142723979+amansahaypersonal@users.noreply.github.com>
2025-06-23 22:53:49 +0530

    Add files via upload

---
+++ rectangle.java
@@ -0,0 +1,15 @@
+class rectangle {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 4; i++) {
+            for (int j = 0; j <= 7; j++) {
+                
+                System.out.print("*");
+              
+
+            }
+           
+            System.out.println("*");
+           
+        }
+    }
+}
